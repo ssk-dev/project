d18f656336904b984a3d20a2cff9606be4bb0725
Sebastian Schulze-Khyi <s.schulze@its-cooperation.de>
2019-08-23 22:30:52 +0200

    - commit to stash

--- root/src/app/app.component.ts
+++ root/src/app/app.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, OnInit, Renderer2} from '@angular/core';
+import {ColorSchemasEnum, ColorSchemaService} from "./components/partials/color-schema/color-schema.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor(private colorSchemaService: ColorSchemaService, private element: ElementRef, private renderer: Renderer2) {
+  }
+
+  ngOnInit(): void {
+    this.colorSchemaService.colorSchema$.subscribe((color_schema) => this.setColorSchemaToHost(color_schema));
+  }
+
+  setColorSchemaToHost(color_schema: ColorSchemasEnum) {
+    this.renderer.setAttribute(this.element.nativeElement, 'color-schema', color_schema)
+  }
 }
--- root/src/app/app.module.ts
+++ root/src/app/app.module.ts
@@ -1,18 +1,57 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './components/layout/header/header.component';
+import {BreadcrumbsComponent} from './components/partials/breadcrumbs/breadcrumbs.component';
+import {SidebarComponent} from './components/layout/sidebar/sidebar.component';
+import {ContentComponent} from './components/layout/content/content.component';
+import {FooterComponent} from './components/layout/footer/footer.component';
+import {ColorSchemaComponent} from './components/partials/color-schema/color-schema.component';
+import {ColorSchemaDirective} from './components/partials/color-schema/color-schema.directive';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatIconModule, MatInputModule,
+  MatMenuModule, MatNativeDateModule,
+  MatTabsModule, MatToolbarModule
+} from "@angular/material";
+import { DatepickerComponent } from './components/partials/material/datepicker/datepicker.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    ContentComponent,
+    FooterComponent,
+    ColorSchemaComponent,
+    ColorSchemaDirective,
+    DatepickerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule
+  ],
+  exports: [
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
